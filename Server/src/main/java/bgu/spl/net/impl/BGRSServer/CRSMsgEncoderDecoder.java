c18679b3487f22d4878dcb694d4dc83a627c21a3
Avivoz <aviv.ozalvo@gmail.com>
2020-12-28 22:54:20 +0200

    added structure for encdec

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/CRSMsgEncoderDecoder.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/CRSMsgEncoderDecoder.java
@@ -1,4 +1,15 @@
 package bgu.spl.net.impl.BGRSServer;
 
-public class CRSMsgEncoderDecoder {
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.impl.BGRSServer.SystemCommands.Commands;
+
+public class CRSMsgEncoderDecoder implements MessageEncoderDecoder<Commands> {
+    public Commands decodeNextByte(byte nextByte) {
+        return null;
+    }
+
+    public byte[] encode(Commands message) {
+        return null;
+    }
+
 }
