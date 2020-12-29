36dec13e3f7d2bd8c3d5d2a5036816583e1696cb
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-29 10:44:50 +0200

    Update Course.java

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/DB/Course.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/DB/Course.java
@@ -1,4 +1,50 @@
 package bgu.spl.net.impl.BGRSServer.DB;
 
+import java.util.ArrayList;
+
 public class Course {
+    //courseNum|courseName|KdamCoursesList|numOfMaxStudents
+    private int courseNum;
+    private String courseName;
+    private ArrayList<Integer> KdamCoursesList;
+    private int numOfMaxStudents;
+
+    public Course(int courseNum, String courseName, ArrayList<Integer> kdamCoursesList, int numOfMaxStudents) {
+        courseNum = this.courseNum;
+        courseName = this.courseName;
+        kdamCoursesList = this.KdamCoursesList;
+        numOfMaxStudents = this.numOfMaxStudents;
+    }
+
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public void setCourseNum(int courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public ArrayList<Integer> getKdamCoursesList() {
+        return KdamCoursesList;
+    }
+
+    public void setKdamCoursesList(ArrayList<Integer> kdamCoursesList) {
+        KdamCoursesList = kdamCoursesList;
+    }
+
+    public int getNumOfMaxStudents() {
+        return numOfMaxStudents;
+    }
+
+    public void setNumOfMaxStudents(int numOfMaxStudents) {
+        this.numOfMaxStudents = numOfMaxStudents;
+    }
 }
