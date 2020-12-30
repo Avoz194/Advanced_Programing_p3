7b94d85ed4358698bba6bb402807ed20c008b66f
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-30 15:29:44 +0200

    Update CourseStat.java

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/CourseStat.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/CourseStat.java
@@ -8,8 +8,6 @@ import java.util.NoSuchElementException;
 
 public class CourseStat extends AdminCommand {
     private int courseNumber;
-    private static final int numOZeroDelimiter = 2;
-    private static final int lengthOfMsg = 4;
 
     public CourseStat(int courseNumber) {
         super(7);
@@ -29,8 +27,5 @@ public class CourseStat extends AdminCommand {
         response.setOptionalMsg(courseData);
         return response;
     }
-    public static int getNumOZeroDelimiter(){return numOZeroDelimiter;}
-    public static int getLengthOfMsg() {
-        return lengthOfMsg;
-    }
+    
 }
