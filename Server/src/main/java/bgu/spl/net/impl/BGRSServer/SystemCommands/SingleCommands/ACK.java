99f94bbc33bdd8d72a8bf3e5a99815d44cf43ea4
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-30 16:25:37 +0200

    Update ACK.java

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/ACK.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/ACK.java
@@ -10,8 +10,27 @@ public class ACK extends ServerCommand {
     }
     public void setOptionalMsg(String msg){optionalMsg=msg;}
 
+      /**
+     * 1) encode the ACK op
+     * 2) encode the messageOpcode
+     * 3) encode the optionalMsg
+     * 4) encode 1 byte 0
+     *
+     * @return array of bytes for the buufer to sent to the client
+     */
     @Override
-    public String encode() {
-        return null;
+    public byte[] encode() {
+        byte[] temp = (optionalMsg + "\0").getBytes();
+        int num = getOpCode();
+        int num2 = messageOpcode;
+        byte[] bytesArr = new byte[temp.length + 4];
+        bytesArr[0] = (byte) ((num >> 8) & 0xFF);
+        bytesArr[1] = (byte) (num & 0xFF);
+        bytesArr[2] = (byte) ((num2 >> 8) & 0xFF);
+        bytesArr[3] = (byte) (num2 & 0xFF);
+        for (int i = 0; i < temp.length; i++) {
+            bytesArr[i + 4] = temp[i];
+        }
+        return bytesArr;
     }
 }
