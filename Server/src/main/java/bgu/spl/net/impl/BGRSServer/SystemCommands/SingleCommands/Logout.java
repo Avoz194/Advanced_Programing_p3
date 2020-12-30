b5be992f86248e81238d5c0ae81c5fbe3cb61827
ransayada <46240258+ransayada@users.noreply.github.com>
2020-12-30 15:31:53 +0200

    Update Logout.java

--- Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/Logout.java
+++ Server/src/main/java/bgu/spl/net/impl/BGRSServer/SystemCommands/SingleCommands/Logout.java
@@ -4,8 +4,7 @@ import bgu.spl.net.impl.BGRSServer.DB.Database;
 import bgu.spl.net.impl.BGRSServer.SystemCommands.*;
 
 public class Logout extends ClientCommand {
-    private static final int numOZeroDelimiter = 2;
-    private static final int lengthOfMsg = 4;
+    
 
     public Logout(){super(4);}
 
@@ -16,8 +15,5 @@ public class Logout extends ClientCommand {
         if (result) return succAction();
         else return error();
     }
-    public static int getNumOZeroDelimiter(){return numOZeroDelimiter;}
-    public static int getLengthOfMsg() {
-        return lengthOfMsg;
-    }
+  
 }
